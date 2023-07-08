045dbfabbd6b7736cdd8ea8c4fa40ddb52203b70
Sergey <gyraserg@mail.ru>
2023-07-08 22:22:03 +0300

    first commit

---
+++ Seminar2.java
@@ -0,0 +1,85 @@
+// Задание
+
+// 1) Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder. Данные для фильтрации приведены ниже в виде json-строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// Параметры для фильтрации:
+// Input:
+
+// {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+// .
+// Итоговый SQL-запрос должен выглядеть как:
+// Output
+// select * from students where name = "Ivanov" and country = "Russia" and city = "Moscow"
+
+
+public class task1 {
+    public static void main(String[] args) throws Exception {
+      String [] list = lib.ReadLineFromFile("dataForSelect.txt"); 
+      System.out.println(list[0]); 
+      StringBuilder resultSelect = LineInList(list[0]);
+      System.out.println(resultSelect);
+    }
+    public static StringBuilder LineInList(String line) {
+        String line1 = line.replace("{", "");
+        String line2 = line1.replace("}", "");
+        String line3 = line2.replaceAll("\"", "");
+        System.out.println(line3);
+        StringBuilder result = new StringBuilder("select * from students where ");
+
+        String [] arrayData = line3.split(", ");
+        for (int i =0; i < arrayData.length; i++) {
+            String[] arrData = arrayData[i].split(":");
+            if(arrData[1].equals("null") == false) {
+                if (i != 0) {
+                    result.append(", ");
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                } else {
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                }
+            }
+            
+        }
+        return result;
+    }
+}
+
+
+
+// 2) Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+
+
+import java.io.IOException;
+import java.util.logging.*;
+public class task2 {
+    public static void main(String[] args) throws IOException {
+        Logger logger = Logger.getLogger(task2.class.getName());
+        FileHandler fh = new FileHandler("logTask2.xml");
+        logger.addHandler(fh);
+        XMLFormatter xml = new XMLFormatter();
+        fh.setFormatter(xml);
+        logger.info("Sort Array");
+        int [] array = {2, 5, 1, -7, 3, 64, 0};
+        lib.printArrayInt(array);
+        int[] newArr = listSort(array);
+        System.out.printf("\n Отсортированный массив: \n");
+        lib.printArrayInt(newArr);
+    }
+    public static int[] listSort(int [] arr){
+        for(int k = 0; k < arr.length-1; k++) {
+            
+            for (int i = 0; i < arr.length - k-1; i++){
+                if (arr[i] > arr[i+1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+    
+}
