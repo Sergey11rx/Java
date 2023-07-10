21ca4adb7130e2b7918b0d0c1d8b7744f741f1e3
Sergey <gyraserg@mail.ru>
2023-07-10 21:27:07 +0300

    first commit

--- Seminar5.java
+++ Seminar5.java
@@ -138,3 +138,50 @@ public class Task_1 {
     }
 
 }
+
+
+
+
+// Пусть дан список сотрудников:
+// Светлана Петрова
+// Кристина Белова
+// Анна Мусина
+// Анна Крутова
+// Иван Юрин
+// Петр Лыков
+// Павел Чернов
+// Петр Чернышов
+// Мария Федорова
+// Марина Светлова
+// Иван Савин
+// Мария Рыкова
+// Марина Лугова
+// Анна Владимирова
+// Иван Мечников
+// Петр Петин
+// Иван Ежов
+
+// Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
+// Отсортировать по убыванию популярности.
+
+// Иван 4, ...
+
+
+import java.util.*;
+
+public class Main { public static void main(String[] args) { List<String> employees = Arrays.asList("Иван Иванов", "Светлана Петрова", "Кристина Белова", "Иван Мечников", "Петр Петин", "Иван Ежов");
+
+    Map<String, Integer> nameCount = new TreeMap<>(Collections.reverseOrder());
+
+    for (String employee : employees) {
+        String name = employee.split(" ")[0];
+        nameCount.put(name, nameCount.getOrDefault(name, 0) + 1);
+    }
+
+    for (Map.Entry<String, Integer> entry : nameCount.entrySet()) {
+        if (entry.getValue() > 1) {
+            System.out.println(entry.getKey() + " - " + entry.getValue() + " повторений");
+        }
+    }
+}
+}
